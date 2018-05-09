84029afe3d765e8413b254145f45ac4482a6f99e
efdssdf <efdssdf@gmail.com>
2018-05-09 16:27:38 +0800

    'fixUrl'

--- message_server.js
+++ message_server.js
@@ -100,7 +100,7 @@ MessageServer.prototype.init_io = function(server,self) {
 
                     for (var item in self.wechat_socket_ids) {
                         if(msg.code == self.wechat_socket_ids[item]){
-                            var url = send_conf.zhifu.replace('ID',doc._id)
+                            var url = send_conf.zhifu.replace('IMAGE',message.pictUrl).replace('WORD',message.token)
                             duanlian.convert_url(url, function (data) {
                                 var str = "返利:"+message.tkCommFee+"  优惠券:" +message.couponAmount+ "  原价:"+message.price
                                     +"\r\n━┉┉┉┉∞┉┉┉┉━┉━┉━\r\n"+"点击链接查看商品\r\n" + data
